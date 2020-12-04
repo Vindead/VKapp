161e453fd879d254cbf9361b579b11b680176014
Kiriill <Vindead123@yandex.ru>
2020-12-05 01:51:12 +0300

    Урок 1 разработки VKapp

---
+++ Урок 1/VKapp/VKapp/LoginController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  VKapp
+//
+//  Created by Kirill Bubnov on 04/12/2020.
+//  Copyright © 2020 Kirill Bubnov. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    
+    @IBOutlet var userNameTextFild: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    
+    
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+       }
+    
+    
+    
+    
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        print(userNameTextFild.text)
+        print(passwordTextField.text)
+    }
+    
+    
+}
+
